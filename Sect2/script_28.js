26982e2735229573c242e9b5f680f382d730a658
gxgarciat <info@gxgarciat.dev>
2022-05-26 17:25:26 +0100

    Sect 2

---
+++ Sect2/script_28.js
@@ -0,0 +1,13 @@
+// Use of The conditional (Ternary operator), because it has 3 parts
+// the condition, the if part and the else part
+
+const age = 19;
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+// Variation
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+
+// This does not replace if statement
+// Good for quick decisions
