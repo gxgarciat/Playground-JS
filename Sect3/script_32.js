11022e7128247fc3ec41107e553f8e0fb42a6d9d
gxgarciat <info@gxgarciat.dev>
2022-05-26 19:56:40 +0100

    Sect 3

---
+++ Sect3/script_32.js
@@ -0,0 +1,8 @@
+'use strict'; // this will add strict mode (secure) to our JS file. It needs to be placed first.
+// without using strict mode, it won't show up the error.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; 
+if (hasDriversLicense) console.log('I can drive');
